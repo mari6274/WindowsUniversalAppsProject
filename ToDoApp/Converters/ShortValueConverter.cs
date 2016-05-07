bf93317ba2209c6bfb72b5d94a6efaebfc632d7c
Mariusz Mączkowski <mari6274@wp.pl>
2016-05-07 21:30:50 +0200

    Styled app

---
+++ ToDoApp/Converters/ShortValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace ToDoApp.Converters
+{
+    class ShortValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var text = (string) value;
+            if (text.Length > 100)
+            {
+                text = text.Remove(100);
+                text += "...";
+            }
+            return text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
